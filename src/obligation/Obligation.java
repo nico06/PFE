14be07109f7316256fce8bb9df447f7896a98b97
Marquez Nicolas <nico_mhr@hotmail.fr>
2015-02-24 17:14:41 +0100

    Démarrage du code

---
+++ src/obligation/Obligation.java
@@ -0,0 +1,14 @@
+package obligation;
+
+import yield.YieldCurve;
+
+public class Obligation {
+	//private YieldCurve yc;
+	
+	private double getYield(YieldCurve yc){
+		
+		return 0;
+		
+	}
+
+}
---
+++ src/yield/YieldCurve.java
@@ -0,0 +1,5 @@
+package yield;
+
+public class YieldCurve {
+
+}
